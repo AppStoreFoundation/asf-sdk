2e42917280af0895dec8d3ea1e2223561f9f14fe
Neurophobic Animal <FredericoSantinho@gmail.com>
2017-10-24 10:13:47 +0100

    Migrated ECKey code to transaction generator module.

---
+++ java/ethereumj/transaction-generator/src/main/java/cm/aptoide/pt/ethereumj/crypto/jce/SpongyCastleProvider.java
@@ -0,0 +1,16 @@
+package cm.aptoide.pt.ethereumj.crypto.jce;
+
+import org.spongycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.Provider;
+
+public final class SpongyCastleProvider {
+
+  private static class Holder {
+    private static final Provider INSTANCE = new BouncyCastleProvider();
+  }
+
+  public static Provider getInstance() {
+    return Holder.INSTANCE;
+  }
+}
