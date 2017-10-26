8498b777a8596490476e5c0887e2bb52780f660d
Neurophobic Animal <FredericoSantinho@gmail.com>
2017-10-26 15:49:28 +0100

    Added sendRawTransaction to EtherscanApi.java.

--- java/ethereumj/transaction-generator/src/main/java/cm/aptoide/pt/ws/etherscan/EtherscanApi.java
+++ java/ethereumj/transaction-generator/src/main/java/cm/aptoide/pt/ws/etherscan/EtherscanApi.java
@@ -1,6 +1,7 @@
 package cm.aptoide.pt.ws.etherscan;
 
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 import rx.Observable;
 
@@ -13,4 +14,7 @@ public interface EtherscanApi {
 
 	@GET("api?module=account&action=balance&tag=latest")
 	Observable<BalanceResponse> getBalance(@Query("address") String address);
+
+	@POST("api?module=proxy&action=eth_sendRawTransaction")
+	Observable<Object> sendRawTransaction(@Query("hex") String rawData);
 }
