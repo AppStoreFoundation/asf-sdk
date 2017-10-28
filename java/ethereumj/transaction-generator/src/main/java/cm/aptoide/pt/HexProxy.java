f7d9d22840673f642795649dbff1869c12352d4f
Neurophobic Animal <FredericoSantinho@gmail.com>
2017-10-28 17:33:32 +0100

    HexProxy.java now allows s = "0".

--- java/ethereumj/transaction-generator/src/main/java/cm/aptoide/pt/HexProxy.java
+++ java/ethereumj/transaction-generator/src/main/java/cm/aptoide/pt/HexProxy.java
@@ -5,6 +5,10 @@ import org.spongycastle.util.encoders.Hex;
 public class HexProxy {
 
 	static byte[] decode(String s) {
+		if (s.charAt(0) == '0') {
+			return new byte[]{0};
+		}
+
 		if (s.length() % 2 != 0) {
 			s = " " + s;
 		}
