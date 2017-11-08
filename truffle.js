268115388624b753775994a575cb2a135a39a77f
makemesteaks <castrofjoao@gmail.com>
2017-11-08 11:31:58 +0000

    added kovan

--- truffle.js
+++ truffle.js
@@ -12,6 +12,14 @@ module.exports = {
                         network_id: 1,
                         gasPrice: 3000000000, // Be careful, this is in Shannon
                         gas: 2000000
+                      },
+              kovan: {
+                        host: "localhost", // Connect to geth on the specified
+                        port: 8545,
+                        from: "0x0000000000000000000000000000000000000000", // change for default address used for deployment
+                        network_id: 42,
+                        gas: 2000000, // Gas limit used for deploys
+                        gasPrice: 3000000000
                       }
             }
 };
