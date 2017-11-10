1302a849ffe768c82839f0ef722fe664a2a8ff3c
Neurophobic Animal <FredericoSantinho@gmail.com>
2017-11-10 09:44:29 +0000

    Gas price is now 24_000_000_000L.

--- java/ethereumj/transaction-generator/src/main/java/cm/aptoide/pt/TransactionFactory.java
+++ java/ethereumj/transaction-generator/src/main/java/cm/aptoide/pt/TransactionFactory.java
@@ -5,7 +5,7 @@ import cm.aptoide.pt.ethereumj.util.ByteUtil;
 
 public class TransactionFactory {
 
-	private static final long gasPrice = 12_000_000_000L;
+	private static final long gasPrice = 24_000_000_000L;
 	private static final long gasLimit = 0xfffff;
 
 	public Transaction createTransaction(byte[] nonce, byte[] gasPrice, byte[] gasLimit, byte[]
@@ -16,9 +16,8 @@ public class TransactionFactory {
 
 	public Transaction createTransaction(int nonce, String receiverAddr, int value, byte[] data, int
 					chainIdForNextBlock) {
-		return this.createTransaction(ByteUtil.intToBytesNoLeadZeroes(nonce),
-						ByteUtil.longToBytesNoLeadZeroes(TransactionFactory.gasPrice),
-						ByteUtil.longToBytesNoLeadZeroes(TransactionFactory.gasLimit),
+		return createTransaction(ByteUtil.intToBytesNoLeadZeroes(nonce),
+						ByteUtil.longToBytesNoLeadZeroes(gasPrice), ByteUtil.longToBytesNoLeadZeroes(gasLimit),
 						HexUtils.decode(receiverAddr), HexUtils.decode(Long.toHexString(value)), data,
 						chainIdForNextBlock);
 	}
