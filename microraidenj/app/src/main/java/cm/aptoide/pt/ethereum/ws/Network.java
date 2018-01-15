62a6b07cb3afccc13c3b10e864e106ae17821eac
Neurophobic Animal <FredericoSantinho@gmail.com>
2018-01-15 14:40:25 +0000

    Added kovan network support.

--- microraidenj/app/src/main/java/cm/aptoide/pt/ethereum/ws/Network.java
+++ microraidenj/app/src/main/java/cm/aptoide/pt/ethereum/ws/Network.java
@@ -2,8 +2,8 @@ package cm.aptoide.pt.ethereum.ws;
 
 public enum Network {
 
-  MAINNET("https://api.etherscan.io/", 1), ROPSTEN("https://ropsten.etherscan.io/", 3), RINKEBY(
-      "https://rinkeby.etherscan.io/", 4),;
+  MAINNET("https://api.etherscan.io/", 1), ROPSTEN("https://ropsten.etherscan.io/", 3), KOVAN(
+      "https://kovan.etherscan.io/", 42),;
 
   private final String host;
   private final int networkId;
