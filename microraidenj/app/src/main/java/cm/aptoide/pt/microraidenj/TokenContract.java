822d6504a8386c371d7e9ee31a79a83b8dabf6e2
Neurophobic Animal <FredericoSantinho@gmail.com>
2018-01-19 15:50:46 +0000

    Added topUp implementation.

--- microraidenj/app/src/main/java/cm/aptoide/pt/microraidenj/TokenContract.java
+++ microraidenj/app/src/main/java/cm/aptoide/pt/microraidenj/TokenContract.java
@@ -40,27 +40,59 @@ public class TokenContract {
         .substring(2, receiver.getValue()
             .length()));
 
-    byte[] data = buildTransferDataField(sender, receiverBytes);
+    return transfer(ecKey, receiver, value, buildTransferDataField(sender, receiverBytes));
+  }
 
+  private boolean transfer(ECKey ecKey, Address receiver, Uint256 value, byte[] data) {
     transferFunction.encode(receiver.getValue(), value.getValue(), data);
 
-    TransactionResultResponse response =
-        ethereumApi.getCurrentNonce(Hex.toHexString(ecKey.getAddress()))
-            .flatMap(nonce -> ethereumApi.call(nonce, ecKey, GAS_PRICE, GAS_LIMIT, tokenAddress,
-                transferFunction.encode(channelManagerContractAddress.getValue(), value.getValue(),
-                    data)))
-            .toBlocking()
-            .first();
+    TransactionResultResponse response = ethereumApi.getCurrentNonce(Hex.toHexString(ecKey.getAddress()))
+        .flatMap(nonce -> ethereumApi.call(nonce, ecKey, GAS_PRICE, GAS_LIMIT, tokenAddress,
+            transferFunction.encode(channelManagerContractAddress.getValue(), value.getValue(), data)))
+        .toBlocking()
+        .first();
 
     System.out.println(TAG + " transfer returned: " + response);
 
     return response != null && response.result != null;
   }
 
+  public boolean topUp(ECKey ecKey, Address receiver, Uint256 value, int blockNumber) {
+    byte[] sender = ecKey.getAddress();
+    byte[] receiverBytes = Hex.decode(receiver.getValue()
+        .substring(2, receiver.getValue()
+            .length()));
+
+    return transfer(ecKey, receiver, value,
+        buildTopUpDataField(sender, receiverBytes, blockNumber));
+  }
+
   private byte[] buildTransferDataField(byte[] sender, byte[] receiver) {
     byte[] result = new byte[sender.length + receiver.length];
     System.arraycopy(sender, 0, result, 0, sender.length);
     System.arraycopy(receiver, 0, result, sender.length, receiver.length);
+    return result;
+  }
+
+  private byte[] buildTopUpDataField(byte[] sender, byte[] receiver, int blockNumber) {
+    byte[] data = buildTransferDataField(sender, receiver);
+    int blockNumberHeaderSize = 4;
+    byte[] result = new byte[data.length + blockNumberHeaderSize];
+
+    System.arraycopy(data, 0, result, 0, data.length);
+    System.arraycopy(buildBlockNumberBytes(blockNumber), 0, result, data.length,
+        blockNumberHeaderSize);
+
+    return result;
+  }
+
+  private byte[] buildBlockNumberBytes(int blockNumber) {
+    byte[] result = new byte[4];
+
+    byte[] decodedNumber = Hex.decode(Integer.toHexString(blockNumber));
+    int length = decodedNumber.length;
+    System.arraycopy(decodedNumber, 0, result, 4 - length, length);
+
     return result;
   }
 }
