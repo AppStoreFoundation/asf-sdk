2f920f55e0b4d136b2e2a8d172bcc01bd63271ed
Neurophobic Animal <FredericoSantinho@gmail.com>
2018-02-28 18:48:04 +0000

    [APPC-91] - Added AsfWeb3jImpl.java implementation.

---
+++ appcoins/appcoins-sdk/src/main/java/com/asf/appcoins/sdk/AsfWeb3jImpl.java
@@ -0,0 +1,65 @@
+package com.asf.appcoins.sdk;
+
+import com.asf.appcoins.sdk.entity.Transaction;
+import com.asf.appcoins.sdk.entity.Transaction.Status;
+import com.asf.appcoins.sdk.entity.factory.TransactionFactory;
+import io.reactivex.Observable;
+import java.math.BigDecimal;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthSendTransaction;
+import org.web3j.protocol.core.methods.response.EthTransaction;
+
+/**
+ * Created by neuro on 26-02-2018.
+ */
+
+final class AsfWeb3jImpl implements AsfWeb3j {
+
+  private final Web3j web3j;
+
+  AsfWeb3jImpl(Web3j web3j) {
+    this.web3j = web3j;
+  }
+
+  @Override public Observable<Long> getNonce(Address address) {
+    return Observable.fromCallable(() -> web3j.ethGetTransactionCount(address.getTypeAsString(),
+        DefaultBlockParameterName.PENDING)
+        .send())
+        .map(ethGetTransactionCount -> ethGetTransactionCount.getTransactionCount()
+            .longValue());
+  }
+
+  @Override public Observable<Long> getGasPrice(Address address) {
+    return Observable.fromCallable(() -> web3j.ethGasPrice()
+        .send())
+        .map(ethGasPrice -> ethGasPrice.getGasPrice()
+            .longValue());
+  }
+
+  @Override public Observable<BigDecimal> getBalance(Address address) {
+    throw new RuntimeException("Not implemented yet");
+  }
+
+  @Override public Observable<String> sendRawTransaction(String rawData) {
+    return Observable.fromCallable(() -> web3j.ethSendRawTransaction(rawData)
+        .send())
+        .map(EthSendTransaction::getTransactionHash);
+  }
+
+  @Override public Observable<Transaction> getTransactionByHash(String txHash) {
+    return Observable.fromCallable(() -> web3j.ethGetTransactionReceipt(txHash)
+        .send())
+        .flatMap(ethGetTransactionReceipt -> {
+          if (ethGetTransactionReceipt == null) {
+            EthTransaction send = web3j.ethGetTransactionByHash(txHash)
+                .send();
+            return Observable.just(TransactionFactory.fromEthTransaction(send, Status.PENDING));
+          } else {
+            return Observable.just(
+                TransactionFactory.fromEthGetTransactionReceipt(ethGetTransactionReceipt));
+          }
+        });
+  }
+}
--- appcoins/appcoins-sdk/src/main/java/com/asf/appcoins/sdk/entity/Transaction.java
+++ appcoins/appcoins-sdk/src/main/java/com/asf/appcoins/sdk/entity/Transaction.java
@@ -3,24 +3,64 @@ package com.asf.appcoins.sdk.entity;
 /**
  * Created by neuro on 26-02-2018.
  */
-
-public class Transaction {
+public final class Transaction {
 
   private final String hash;
   private final String from;
   private final String to;
   private final String value;
-  private final State state;
+  private final Status status;
+  private final String contractAddress;
 
-  public Transaction(String hash, String from, String to, String value, State state) {
+  public Transaction(String hash, String from, String to, String value, Status status,
+      String contractAddress) {
     this.hash = hash;
     this.from = from;
     this.to = to;
     this.value = value;
-    this.state = state;
+    this.status = status;
+    this.contractAddress = contractAddress;
   }
 
-  public enum State {
-    PENDING, ACCEPTED, FAILED
+  public String getHash() {
+    return hash;
+  }
+
+  public String getFrom() {
+    return from;
+  }
+
+  public String getTo() {
+    return to;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public String getContractAddress() {
+    return contractAddress;
+  }
+
+  public enum Status {
+    PENDING(-1), ACCEPTED(1), FAILED(0),;
+
+    private final String value;
+
+    Status(int value) {
+      this.value = Integer.toString(value);
+    }
+
+    Status(String value) {
+      this.value = value;
+    }
+
+    public String getValue() {
+      return value;
+    }
   }
 }
