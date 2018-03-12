3b3c73cfe7545e57ca9ca2135faac53467d78c4d
root <root@ip-172-31-52-247.ec2.internal>
2018-03-12 11:22:59 +0000

    Added Ropsten network to truffle config file

--- deploy/truffle.js
+++ deploy/truffle.js
@@ -20,6 +20,14 @@ module.exports = {
                         network_id: 42,
                         gas: 2000000, // Gas limit used for deploys
                         gasPrice: 3000000000
+                     },
+	      ropsten: {
+                        host: "localhost", // Connect to geth on the specified
+                        port: 8545,
+                        from: "0x2c30194bd2e7b6b8ff1467c5af1650f53cd231be", // change for default address used for deployment
+                        network_id: 3,
+                        gas: 2000000, // Gas limit used for deploys
+                        gasPrice: 20000000000
                       }
-            }
+             }
 };
