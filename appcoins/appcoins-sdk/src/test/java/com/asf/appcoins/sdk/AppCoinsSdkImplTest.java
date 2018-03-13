2022322425d6f11796474ae8fd2daa2635888912
Neurophobic Animal <FredericoSantinho@gmail.com>
2018-03-13 18:44:50 +0000

    Minor changes in AppCoinsSdkImplTest.java.

--- appcoins/appcoins-sdk/src/test/java/com/asf/appcoins/sdk/AppCoinsSdkImplTest.java
+++ appcoins/appcoins-sdk/src/test/java/com/asf/appcoins/sdk/AppCoinsSdkImplTest.java
@@ -29,12 +29,11 @@ public class AppCoinsSdkImplTest {
     AppCoinsSdk appCoinsSdk = new AppCoinsSdkBuilder(developerAddress).withSkus(buildSkus())
             .createAppCoinsSdk();
 
-    String uriString =
-        UriBuilder.buildUriString("0xab949343E6C369C6B17C7ae302c1dEbD4B7B61c3", BigDecimal.ONE,
-            developerAddress, "com.sku.id", 3);
+    String uriString = UriBuilder.buildUriString("0xab949343E6C369C6B17C7ae302c1dEbD4B7B61c3",
+        new BigDecimal("1000000000000000000"), developerAddress, "com.cenas.product", 3);
 
     Assert.assertThat(uriString,
-        is("ethereum:0xab949343E6C369C6B17C7ae302c1dEbD4B7B61c3@3/buy?uint256=1&developerAddress=0x4fbcc5ce88493c3d9903701c143af65f54481119&data=0x636f6d2e736b752e6964"));
+        is("ethereum:0xab949343E6C369C6B17C7ae302c1dEbD4B7B61c3@3/buy?uint256=1000000000000000000&address=0x4fbcc5ce88493c3d9903701c143af65f54481119&data=0x636f6d2e63656e61732e70726f64756374"));
   }
 
   private List<SKU> buildSkus() {
