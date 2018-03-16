db2c0e2d3246a9eddb4e3d14c383ef9f0ca4bb08
Neurophobic Animal <FredericoSantinho@gmail.com>
2018-03-16 18:44:03 +0000

    [APPC-90] - PaymentService.java know allows to intent wallet installation.

--- appcoins/appcoins-sdk/src/main/java/com/asf/appcoins/sdk/payment/PaymentService.java
+++ appcoins/appcoins-sdk/src/main/java/com/asf/appcoins/sdk/payment/PaymentService.java
@@ -1,14 +1,22 @@
 package com.asf.appcoins.sdk.payment;
 
+import android.R.drawable;
+import android.R.string;
 import android.app.Activity;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import com.asf.appcoins.sdk.AsfWeb3j;
 import com.asf.appcoins.sdk.SkuManager;
 import com.asf.appcoins.sdk.entity.Transaction;
 import com.asf.appcoins.sdk.entity.Transaction.Status;
 import com.asf.appcoins.sdk.util.UriBuilder;
+import com.asf.appcoins.sdk.wallet.AndroidUtils;
 import io.reactivex.Observable;
+import io.reactivex.Single;
+import io.reactivex.functions.Consumer;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,6 +34,7 @@ public final class PaymentService {
   private final String developerAddress;
   private final Map<String, PaymentDetails> payments;
   private final AsfWeb3j asfWeb3j;
+
   private PaymentDetails onGoingPayment;
 
   public PaymentService(int networkId, SkuManager skuManager, String developerAddress,
@@ -38,26 +47,63 @@ public final class PaymentService {
   }
 
   public void buy(String skuId, Activity activity, int defaultRequestCode) {
-    Intent intent = new Intent(Intent.ACTION_VIEW);
-
     BigDecimal amount = skuManager.getSkuAmount(skuId);
     BigDecimal total = amount.multiply(BigDecimal.TEN.pow(DECIMALS));
 
+    Intent intent = buildPaymentIntent(skuId, total);
+
+    if (AndroidUtils.hasHandlerAvailable(intent, activity)) {
+      if (payments.containsKey(skuId)) {
+        throw new IllegalArgumentException(
+            "Pending buy action with the same sku found! Did you forget to consume the former?");
+      } else {
+        onGoingPayment = new PaymentDetails(PaymentStatus.FAIL, skuId,
+            new Transaction(null, null, developerAddress, total.toString(), Status.PENDING));
+        payments.put(skuId, onGoingPayment);
+
+        activity.startActivityForResult(intent, defaultRequestCode);
+      }
+    } else {
+      showWalletInstallDialog(activity).filter(aBoolean -> aBoolean)
+          .doOnSuccess(gotoStore(activity))
+          .subscribe(aBoolean -> {
+          }, Throwable::printStackTrace);
+    }
+  }
+
+  @NonNull private Consumer<Boolean> gotoStore(Activity activity) {
+    return aBoolean -> {
+      String appPackageName = "com.asf.wallet";
+      try {
+        activity.startActivity(
+            new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+      } catch (android.content.ActivityNotFoundException anfe) {
+        activity.startActivity(new Intent(Intent.ACTION_VIEW,
+            Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+      }
+    };
+  }
+
+  private Single<Boolean> showWalletInstallDialog(Context context) {
+    return Single.create(emitter -> {
+      Builder builder;
+      builder = new Builder(context);
+      builder.setTitle("Appc Wallet Missing")
+          .setMessage("Do you want to install the ASF wallet?")
+          .setPositiveButton(string.yes, (dialog, which) -> emitter.onSuccess(true))
+          .setNegativeButton(string.no, (dialog, which) -> emitter.onSuccess(false))
+          .setIcon(drawable.ic_dialog_alert)
+          .show();
+    });
+  }
+
+  @NonNull private Intent buildPaymentIntent(String skuId, BigDecimal total) {
+    Intent intent = new Intent(Intent.ACTION_VIEW);
     Uri data =
         UriBuilder.buildUri("0xab949343e6c369c6b17c7ae302c1debd4b7b61c3", skuId, networkId, total,
             developerAddress);
     intent.setData(data);
-
-    if (payments.containsKey(skuId)) {
-      throw new IllegalArgumentException(
-          "Pending buy action with the same sku found! Did you forget to consume the former?");
-    } else {
-      onGoingPayment = new PaymentDetails(PaymentStatus.FAIL, skuId,
-          new Transaction(null, null, developerAddress, total.toString(), Status.PENDING));
-      payments.put(skuId, onGoingPayment);
-
-      activity.startActivityForResult(intent, defaultRequestCode);
-    }
+    return intent;
   }
 
   public Observable<PaymentDetails> getPaymentDetails(String skuId) {
