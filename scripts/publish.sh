4544bbdb5602ce905bfdf64cb6773581cab44046
Neurophobic Animal <NeurophobicAnimal@gmail.com>
2018-04-14 16:06:15 +0100

    [APPC-207] - Updated publish.sh.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-./gradlew :appcoins-sdk:clean :appcoins-sdk-iab:clean :appcoins-sdk-ads:clean
-./gradlew :appcoins-sdk:assemble :appcoins-sdk-iab:assemble :appcoins-sdk-ads:assemble
+cd ..
+./gradlew :appcoins-sdk:clean :appcoins-sdk-iab:clean :appcoins-sdk-ads:clean :appcoins-sdk-core:clean
+./gradlew :appcoins-sdk:assemble :appcoins-sdk-iab:assemble :appcoins-sdk-ads:assemble :appcoins-sdk-core:assemble
 ./gradlew publish bintrayUpload
