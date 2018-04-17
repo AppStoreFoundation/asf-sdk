8d4ccc490f0f4efc091901bc90cbd68142e41af0
Joao Raimundo <joao.raimundo@aptoide.com>
2018-04-17 19:55:35 +0100

    [APPC-192] Set toolbox to main network

--- toolbox/src/main/java/com/asf/appcoins/toolbox/App.java
+++ toolbox/src/main/java/com/asf/appcoins/toolbox/App.java
@@ -13,8 +13,8 @@ public class App extends Application {
   @Override public void onCreate() {
     super.onCreate();
 
-    AppCoinsIabSingleton.create(DEVELOPER_ADDRESS, buildSkus(), true);
-    AdvertisementSdkSingleton.create(this, true);
+    AppCoinsIabSingleton.create(DEVELOPER_ADDRESS, buildSkus(), false);
+    AdvertisementSdkSingleton.create(this, false);
     AdvertisementSdkSingleton.getAdsSdk().init(this);
   }
 
