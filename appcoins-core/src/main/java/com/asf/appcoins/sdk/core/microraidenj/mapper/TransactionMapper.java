380cf3779212f20a0a6f189b7407b2242239d4bb
Neurophobic Animal <NeurophobicAnimal@gmail.com>
2018-05-24 09:21:45 +0100

    [APPC-377] - Fix bug in TransactionMapper.java when decoding data field.

--- appcoins-core/src/main/java/com/asf/appcoins/sdk/core/microraidenj/mapper/TransactionMapper.java
+++ appcoins-core/src/main/java/com/asf/appcoins/sdk/core/microraidenj/mapper/TransactionMapper.java
@@ -15,7 +15,8 @@ public class TransactionMapper {
     BigInteger blockNumber = transactionReceipt.getBlockNumber();
     byte[] data = Hex.decode(transactionReceipt.getLogs()
         .get(0)
-        .getData());
+        .getData()
+        .substring(2));
     BigInteger gasUsed = transactionReceipt.getGasUsed();
     String transactionHash = transactionReceipt.getTransactionHash();
     BigInteger transactionIndex = transactionReceipt.getTransactionIndex();
