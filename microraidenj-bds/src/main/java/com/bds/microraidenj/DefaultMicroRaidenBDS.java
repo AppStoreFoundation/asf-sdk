93ea549856cbffe6b2ad96155ce8d7547da51a6a
Neurophobic Animal <NeurophobicAnimal@gmail.com>
2018-06-14 09:55:05 +0100

    [APPC-378] - MicroRaidenBDS.java now supports listTransactions based on sender address.

--- microraidenj-bds/src/main/java/com/bds/microraidenj/DefaultMicroRaidenBDS.java
+++ microraidenj-bds/src/main/java/com/bds/microraidenj/DefaultMicroRaidenBDS.java
@@ -5,7 +5,9 @@ import com.asf.microraidenj.type.Address;
 import com.bds.microraidenj.channel.BDSChannel;
 import com.bds.microraidenj.channel.BDSChannelImpl;
 import com.bds.microraidenj.ws.BDSMicroRaidenApi;
+import com.bds.microraidenj.ws.ChannelHistoryResponse;
 import com.bds.microraidenj.ws.ListAllChannelsResponse;
+import com.bds.microraidenj.ws.Type;
 import ethereumj.crypto.ECKey;
 import io.reactivex.Single;
 import java.math.BigInteger;
@@ -49,6 +51,13 @@ public final class DefaultMicroRaidenBDS implements MicroRaidenBDS {
         .flatMapIterable(ListAllChannelsResponse::getResult)
         .map(result -> map(senderECKey, result))
         .toList();
+  }
+
+  @Override public Single<List<ChannelHistoryResponse.MicroTransaction>> listTransactions(
+      Address senderAddress) {
+    return bdsMicroRaidenApi.channelHistory(senderAddress, null, null, Type.Payment)
+        .map(ChannelHistoryResponse::getResult)
+        .singleOrError();
   }
 
   private BDSChannel map(ECKey senderECKey, ListAllChannelsResponse.Result result) {
--- microraidenj-bds/src/main/java/com/bds/microraidenj/MicroRaidenBDS.java
+++ microraidenj-bds/src/main/java/com/bds/microraidenj/MicroRaidenBDS.java
@@ -2,6 +2,7 @@ package com.bds.microraidenj;
 
 import com.asf.microraidenj.type.Address;
 import com.bds.microraidenj.channel.BDSChannel;
+import com.bds.microraidenj.ws.ChannelHistoryResponse;
 import ethereumj.crypto.ECKey;
 import io.reactivex.Single;
 import java.math.BigInteger;
@@ -9,8 +10,9 @@ import java.util.List;
 
 public interface MicroRaidenBDS {
 
-  Single<BDSChannel> createChannel(ECKey senderECKey, Address receiverAddress,
-      BigInteger balance);
+  Single<BDSChannel> createChannel(ECKey senderECKey, Address receiverAddress, BigInteger balance);
 
   Single<List<BDSChannel>> listChannels(ECKey senderECKey, boolean closed);
+
+  Single<List<ChannelHistoryResponse.MicroTransaction>> listTransactions(Address senderAddress);
 }
