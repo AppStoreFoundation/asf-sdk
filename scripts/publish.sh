3a2537ebd89b43fbe685c6332516fd532f41ddba
Joao Raimundo <joao.raimundo@aptoide.com>
2018-06-26 11:11:20 +0100

    [APPC 421] Updated build script and gradle build for contract proxy sdk

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ..
-./gradlew :appcoins:clean :appcoins-iab:clean :appcoins-ads:clean :appcoins-core:clean :microraidenj:clean :microraidenj-bds:clean :ethereumj-android:clean
-./gradlew :appcoins:assemble :appcoins-iab:assemble :appcoins-ads:assemble :appcoins-core:assemble :microraidenj:assemble :microraidenj-bds:assemble :ethereumj-android:assemble
+./gradlew :appcoins:clean :appcoins-iab:clean :appcoins-ads:clean :appcoins-core:clean :microraidenj:clean :microraidenj-bds:clean :ethereumj-android:clean :appcoins-contract-proxy:clean
+./gradlew :appcoins:assemble :appcoins-iab:assemble :appcoins-ads:assemble :appcoins-core:assemble :microraidenj:assemble :microraidenj-bds:assemble :ethereumj-android:assemble :appcoins-contract-proxy:assemble
 ./gradlew publish bintrayUpload
