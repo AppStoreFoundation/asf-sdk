67d96845db61994def51e4c09e6a2372837bb54e
Trinkes <fabiojscosta1583@gmail.com>
2018-08-21 19:37:28 +0100

    [POA/APPC-573] - get campaign id from bds backend instead of blockchain

--- appcoins-ads/src/main/java/com/asf/appcoins/sdk/ads/ip/IpApi.java
+++ appcoins-ads/src/main/java/com/asf/appcoins/sdk/ads/ip/IpApi.java
@@ -1,6 +1,8 @@
 package com.asf.appcoins.sdk.ads.ip;
 
-import io.reactivex.Observable;
+import com.asf.appcoins.sdk.core.util.LogInterceptor;
+import io.reactivex.Single;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.jackson.JacksonConverterFactory;
@@ -8,16 +10,18 @@ import retrofit2.http.GET;
 
 public interface IpApi {
 
-  String ENDPOINT = "http://ip-api.com/";
+  String ENDPOINT = "http://52.209.250.255/";
 
   static IpApi create() {
+    OkHttpClient.Builder builder = new OkHttpClient.Builder().addInterceptor(new LogInterceptor());
     Retrofit retrofit = new Retrofit.Builder().addConverterFactory(JacksonConverterFactory.create())
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .client(builder.build())
         .baseUrl(ENDPOINT)
         .build();
 
     return retrofit.create(IpApi.class);
   }
 
-  @GET("json") Observable<IpResponse> myIp();
+  @GET("exchange/countrycode") Single<IpResponse> getCountry();
 }
