3211986425198294b29dd8ca68860dafa80a5393
Joao Raimundo <joao.raimundo@aptoide.com>
2018-09-19 16:35:42 +0100

    [APPC-589] 1-Add minor changes to build script so the build is contains dev reference. 2-Change publish.gradle to include a possible suffix on the version used to be automaticaly defined on jenkins builds.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-cd ..
+# To run this script locally, change this variable to the location path of the project
+#FOLDER="/path/to/project/location"
+
+pushd ${FOLDER};
+
+# Add '.dev' on the version name. Non dev builds are done though jenkins build only
+export BUILD_NAME_SUFFIX=".dev"
+
 ./gradlew :appcoins:clean :appcoins-iab:clean :appcoins-ads:clean :appcoins-core:clean :microraidenj:clean :microraidenj-bds:clean :ethereumj-android:clean :appcoins-contract-proxy:clean
 ./gradlew :appcoins:assemble :appcoins-iab:assemble :appcoins-ads:assemble :appcoins-core:assemble :microraidenj:assemble :microraidenj-bds:assemble :ethereumj-android:assemble :appcoins-contract-proxy:assemble
 ./gradlew publish bintrayUpload
+
+popd
