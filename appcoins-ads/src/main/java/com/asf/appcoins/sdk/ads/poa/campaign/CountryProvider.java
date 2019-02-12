464aca6d5c12671441279faa3173d85f8b831734
Bruno Varela <bruno.varela@aptoide.com>
2019-02-12 12:49:47 +0000

    Changes
    -Removed Unused files

--- appcoins-ads/src/main/java/com/asf/appcoins/sdk/ads/poa/campaign/CountryProvider.java
+++
@@ -1,5 +0,0 @@
-package com.asf.appcoins.sdk.ads.poa.campaign;
-
-public interface CountryProvider {
-  String getCountryCode();
-}
--- appcoins-ads/src/main/java/com/asf/appcoins/sdk/ads/poa/manager/WalletUtils.java
+++ appcoins-ads/src/main/java/com/asf/appcoins/sdk/ads/poa/manager/WalletUtils.java
@@ -24,38 +24,11 @@ public class WalletUtils {
     }
   }
 
-/*
-//duvidas
-  public static Single<Boolean> promptToInstallWallet(Activity activity, String message) {
-    return showWalletInstallDialog(activity, message).doOnSuccess(aBoolean -> {
-      if (aBoolean) {
-        gotoStore(activity);
-      }
-    });
-  }
-*/
-
   public static void promptToInstallWallet(Activity activity, String message,
       DialogVisibleListener dialogVisibleListener) {
     showWalletInstallDialog(activity, message, activity, dialogVisibleListener);
   }
-/*
-  //duvidas
-  private static Single<Boolean> showWalletInstallDialog(Context context, String message) {
-    return Single.create(emitter -> {
-      AlertDialog.Builder builder;
-      builder = new AlertDialog.Builder(context);
-      builder.setTitle(R.string.wallet_missing)
-          .setMessage(message)
-          .setPositiveButton(R.string.install, (dialog, which) -> emitter.onSuccess(true))
-          .setNegativeButton(R.string.skip, (dialog, which) -> emitter.onSuccess(false))
-          .setIcon(android.R.drawable.ic_dialog_alert)
-          .show();
-    });
-  }
-*/
 
-  //duvidas
   private static void showWalletInstallDialog(Context context, String message, Activity activity,
       DialogVisibleListener dialogVisibleListener) {
 
@@ -82,20 +55,4 @@ public class WalletUtils {
     builder.show();
   }
 
-/*
-    @NonNull private static void gotoStore(Context activity) {
-      try {
-        activity.startActivity(
-            new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + walletPackageName)));
-      } catch (android.content.ActivityNotFoundException anfe) {
-        activity.startActivity(new Intent(Intent.ACTION_VIEW,
-            Uri.parse("https://play.google.com/store/apps/details?id=" + walletPackageName)));
-      }
-    }
-*/
-/*
-  private static void gotoStore(Context activity) {
-
-  }
-  */
 }
