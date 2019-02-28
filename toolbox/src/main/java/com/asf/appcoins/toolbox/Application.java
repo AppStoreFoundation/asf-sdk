5a320329c3e8edd3f98ed0f683ca5030d6082424
Bruno Varela <bruno.varela@aptoide.com>
2019-02-28 09:31:38 +0000

    Chages
    - Rename package name
    - Remove comments

--- toolbox/src/main/java/com/asf/appcoins/toolbox/Application.java
+++ toolbox/src/main/java/com/asf/appcoins/toolbox/Application.java
@@ -9,8 +9,6 @@ public class Application extends android.app.Application {
 
     public final static String developerAddress = "0x75e8BdCf84B9bd585753dB64A8637A52Bd46f851";
 
-    // public final static String developerAddress = "0x5fe8f9d5e7bab4b94fa441b871683d124da270f1";
-
     private static AppCoinsAds adsSdk;
 
     @Override public void onCreate() {
