387bd1ead206a0dad9a1b5eab08cb6eaff98fead
Bruno Varela <bruno.varela@aptoide.com>
2019-04-24 11:19:52 +0100

    Patch changes

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ..
 # Add '.dev' on the version name. Non dev builds are done though jenkins build only
-#export BUILD_NAME_SUFFIX=".dev"
+export BUILD_NAME_SUFFIX=".dev"
 
-./gradlew :appcoins-ads:clean
-./gradlew :appcoins-ads:assemble
+./gradlew :appcoins:clean :android-appcoins-billing:clean :appcoins-billing:clean :appcoins-ads:clean
+./gradlew :appcoins:assemble :android-appcoins-billing:assemble :appcoins-billing:assemble :appcoins-ads:assemble
 ./gradlew publish bintrayUpload
