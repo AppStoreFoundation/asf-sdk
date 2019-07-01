aee639a939228034175b98bb4fd7bee4a450aff0
Bruno Varela <bruno.varela@aptoide.com>
2019-07-01 16:58:24 +0100

    Changes to a billing final version
    - Change the way Wallet utils gets the activity.

--- android-appcoins-billing/src/main/java/com/appcoins/sdk/billing/helpers/WalletUtils.java
+++ android-appcoins-billing/src/main/java/com/appcoins/sdk/billing/helpers/WalletUtils.java
@@ -2,30 +2,29 @@ package com.appcoins.sdk.billing.helpers;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.util.ArrayMap;
 import android.util.Log;
 import com.appcoins.sdk.android.billing.BuildConfig;
-import com.appcoins.sdk.android.billing.R;
-import java.lang.reflect.Field;
+import java.lang.ref.WeakReference;
 
 public class WalletUtils {
 
+  private static final String TAG = AppcoinsBillingStubHelper.class.getSimpleName();
   public static String walletPackageName = BuildConfig.BDS_WALLET_PACKAGE_NAME;
   public static String aptoidePackageName = BuildConfig.APTOIDE_PACKAGE_NAME;
 
-  public static Context context;
+  public static WeakReference<Activity> context;
 
-  public static void setContext(Context cont) {
-    context = cont;
+  public static void setContext(Activity cont) {
+    context = new WeakReference<>(cont);
   }
 
   public static boolean hasWalletInstalled() {
-    PackageManager packageManager = context.getPackageManager();
+    PackageManager packageManager = context.get()
+        .getPackageManager();
 
     try {
       packageManager.getPackageInfo(walletPackageName, 0);
@@ -37,7 +36,8 @@ public class WalletUtils {
 
   public static boolean hasAptoideInstalled() {
 
-    PackageManager packageManager = context.getPackageManager();
+    PackageManager packageManager = context.get()
+        .getPackageManager();
     try {
       return packageManager.getApplicationInfo(aptoidePackageName, 0).enabled;
     } catch (PackageManager.NameNotFoundException e) {
@@ -64,23 +64,24 @@ public class WalletUtils {
 
     AlertDialog.Builder builder;
     builder = new AlertDialog.Builder(act);
-    builder.setTitle(R.string.wallet_missing);
-    builder.setMessage(act.getString(R.string.install_wallet_from_iab));
-    Log.d("String name: ", act.getString(R.string.install_wallet_from_iab));
+    builder.setTitle("APPC Wallet Missing");
+    builder.setMessage("To complete your purchase, you have to install an AppCoins wallet");
+    Log.d("String name: ", "To complete your purchase, you have to install an AppCoins wallet");
 
-    builder.setPositiveButton(R.string.install, new DialogInterface.OnClickListener() {
+    builder.setPositiveButton("INSTALL", new DialogInterface.OnClickListener() {
       @Override public void onClick(DialogInterface dialog, int which) {
         OnInstallClickAction(act, appStoreIntent);
       }
     });
 
-    builder.setNegativeButton(R.string.skip, new DialogInterface.OnClickListener() {
+    builder.setNegativeButton("SKIP", new DialogInterface.OnClickListener() {
       @Override public void onClick(DialogInterface dialogInterface, int i) {
         dialogInterface.cancel();
       }
     });
 
     builder.setIcon(android.R.drawable.ic_dialog_alert);
+    Log.d(TAG, "promptToInstallWallet: builder.show()");
     builder.show();
   }
 
@@ -103,24 +104,7 @@ public class WalletUtils {
     }
   }
 
-  public static Activity getActivity() throws Exception {
-    Class activityThreadClass = Class.forName("android.app.ActivityThread");
-    Object activityThread = activityThreadClass.getMethod("currentActivityThread")
-        .invoke(null);
-    Field activitiesField = activityThreadClass.getDeclaredField("mActivities");
-    activitiesField.setAccessible(true);
-    ArrayMap activities = (ArrayMap) activitiesField.get(activityThread);
-    for (Object activityRecord : activities.values()) {
-      Class activityRecordClass = activityRecord.getClass();
-      Field pausedField = activityRecordClass.getDeclaredField("paused");
-      pausedField.setAccessible(true);
-      if (!pausedField.getBoolean(activityRecord)) {
-        Field activityField = activityRecordClass.getDeclaredField("activity");
-        activityField.setAccessible(true);
-        Activity activity = (Activity) activityField.get(activityRecord);
-        return activity;
-      }
-    }
-    return null;
+  public static Activity getActivity() {
+    return context.get();
   }
 }
