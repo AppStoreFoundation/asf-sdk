62c6b46b6c884367bb5fb872b0c27589104cc14f
Bruno Varela <bruno.varela@aptoide.com>
2019-08-05 09:31:26 +0100

    Code review
    Comment on the suffix of the publication

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ..
 # Add '.dev' on the version name. Non dev builds are done though jenkins build only
-export BUILD_NAME_SUFFIX=".dev"
+#export BUILD_NAME_SUFFIX=".dev"
 
 ./gradlew :appcoins:clean :android-appcoins-billing:clean :appcoins-billing:clean :appcoins-ads:clean
 ./gradlew :appcoins:assemble :android-appcoins-billing:assemble :appcoins-billing:assemble :appcoins-ads:assemble
