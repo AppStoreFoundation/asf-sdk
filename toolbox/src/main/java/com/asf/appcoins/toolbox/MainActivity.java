08e410e236d767496d75fb4affcbd3f7d2cbbcd0
José <jose.messejana@gmail.com>
2020-01-22 15:08:54 +0000

    [APPC-1561] Removed null initialization

--- toolbox/src/main/java/com/asf/appcoins/toolbox/MainActivity.java
+++ toolbox/src/main/java/com/asf/appcoins/toolbox/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends Activity {
 
   private static final String TAG = MainActivity.class.getSimpleName();
   private AppcoinsBillingClient cab;
-  private String token = null;
+  private String token;
   private AppCoinsBillingStateListener listener;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
