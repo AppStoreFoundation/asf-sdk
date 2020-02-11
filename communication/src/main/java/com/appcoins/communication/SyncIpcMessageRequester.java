239cfaa6faea9fc724e8454060d3fafad7f3a7f9
Trinkes <fabiojscosta1583@gmail.com>
2020-02-11 14:41:32 +0000

    update documentation
    Signed-off-by: trinkes <fabiojscosta1583@gmail.com>

--- communication/src/main/java/com/appcoins/communication/SyncIpcMessageRequester.java
+++ communication/src/main/java/com/appcoins/communication/SyncIpcMessageRequester.java
@@ -12,9 +12,9 @@ public interface SyncIpcMessageRequester {
    *
    * @throws InterruptedException if the current thread is interrupted while waiting for the
    * response from processor application
-   * @throws MainThreadException Since this is a blocker method and we use activities(which
-   * default methods run on main thread) you can't use main thread to send messages, it would
-   * cause a deadlock
+   * @throws MainThreadException Since this is a blocker method and we use activities to
+   * communicate(which default methods run on main thread) you can't use main thread to send
+   * messages, it would cause a deadlock
    */
   Parcelable sendMessage(int methodId, Parcelable arguments)
       throws InterruptedException, MainThreadException;
