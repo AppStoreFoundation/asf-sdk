586b9ce43580f5876e87c76488ff3b414d43434d
José <jose.messejana@gmail.com>
2020-02-18 11:03:25 +0000

    [APPC-1634] Fixed bug in millis to second conversion. Added time unit to variable name

--- android-appcoins-billing/src/main/java/com/appcoins/sdk/billing/SharedPreferencesRepository.java
+++ android-appcoins-billing/src/main/java/com/appcoins/sdk/billing/SharedPreferencesRepository.java
@@ -8,7 +8,7 @@ public class SharedPreferencesRepository {
 
   private static final String WALLET_ID_KEY = "WALLET_ID";
   private static final String MAX_BONUS_KEY = "MAX_BONUS";
-  private static final String MAX_BONUS_TTL_KEY = "MAX_BONUS_TTL";
+  private static final String MAX_BONUS_TTL_SECONDS_KEY = "MAX_BONUS_TTL";
   private SharedPreferences sharedPreferences;
 
   public SharedPreferencesRepository(Context context) {
@@ -36,15 +36,16 @@ public class SharedPreferencesRepository {
           .putInt(MAX_BONUS_KEY, maxBonus)
           .apply();
       sharedPreferences.edit()
-          .putLong(MAX_BONUS_TTL_KEY, System.currentTimeMillis() * 1000)
+          .putLong(MAX_BONUS_TTL_SECONDS_KEY, System.currentTimeMillis() / 1000)
           .apply();
     }
   }
 
   public boolean hasSavedBonus(long timeInMillis) {
     if (sharedPreferences.contains(MAX_BONUS_KEY)) {
-      if (sharedPreferences.contains(MAX_BONUS_TTL_KEY)) {
-        return (timeInMillis * 1000 - sharedPreferences.getLong(MAX_BONUS_TTL_KEY, 0)) < 86400 * 30;
+      if (sharedPreferences.contains(MAX_BONUS_TTL_SECONDS_KEY)) {
+        return (timeInMillis / 1000 - sharedPreferences.getLong(MAX_BONUS_TTL_SECONDS_KEY, 0))
+            < 86400 * 30;
       }
     }
     return false;
