21ea1e41abf3ce6e7e24446aa324dcd7bcddef6f
José <jose.messejana@gmail.com>
2020-02-20 17:35:54 +0000

    [APPC-1623] Applied improvement to cafeBazaar flow on PoA

--- appcoins-ads/src/main/java/com/asf/appcoins/sdk/ads/poa/manager/WalletUtils.java
+++ appcoins-ads/src/main/java/com/asf/appcoins/sdk/ads/poa/manager/WalletUtils.java
@@ -71,6 +71,8 @@ public class WalletUtils {
     if (isAppInstalled(com.appcoins.billing.sdk.BuildConfig.CAFE_BAZAAR_PACKAGE_NAME,
         context.getPackageManager()) || userFromIran(getUserCountry(context))) {
       iabAction = getIabAction();
+    } else {
+      cafeBazaarWalletAvailable = false;
     }
     Intent serviceIntent = new Intent(iabAction);
 
