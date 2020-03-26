b316acbe4d7ad6af31c2f2feedde69379aa4e2cb
José <jose.messejana@gmail.com>
2020-03-26 15:31:13 +0000

    Fixed staging scripts

--- scripts/publish_staging.sh
+++ scripts/publish_staging.sh
@@ -5,5 +5,5 @@ export BUILD_NAME_SUFFIX=".staging"
 export BUILD_TYPE_NAME="release"
 
 ./gradlew :appcoins:clean :android-appcoins-billing:clean :appcoins-billing:clean :appcoins-ads:clean :appcoins-adyen:clean :communication:clean
-./gradlew :appcoins:assemble :android-appcoins-billing:assemble :appcoins-billing:assemble :appcoins-ads:assemble :appcoins-adyen:assemble :communication:assembleDebug
+./gradlew :appcoins:assemble :android-appcoins-billing:assemble :appcoins-billing:assemble :appcoins-ads:assemble :appcoins-adyen:assemble :communication:assemble
 ./gradlew publish bintrayUpload
