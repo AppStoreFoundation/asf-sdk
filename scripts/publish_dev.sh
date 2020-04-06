944cb1f555b7adeab66ae67bdb7786ea40bbe5ea
José <jose.messejana@gmail.com>
2020-04-06 18:24:12 +0100

    [Spike-APPC-1695] Typo

--- scripts/publish_dev.sh
+++ scripts/publish_dev.sh
@@ -5,5 +5,5 @@ export BUILD_NAME_SUFFIX=".dev"
 export BUILD_TYPE_NAME="debug"
 
 ./gradlew :appcoins:clean :android-appcoins-billing:clean :appcoins-billing:clean :appcoins-ads:clean :appcoins-adyen:clean :communication:clean :appcoins-lifecycle:clean
-./gradlew :appcoins:assembleDebug :android-appcoins-billing:assembleDebug :appcoins-billing:assembleDebug :appcoins-ads:assembleDebug :appcoins-adyen:assembleDebug :communication:assembleDebug :appcoins-lifecyle:assembleDebug
+./gradlew :appcoins:assembleDebug :android-appcoins-billing:assembleDebug :appcoins-billing:assembleDebug :appcoins-ads:assembleDebug :appcoins-adyen:assembleDebug :communication:assembleDebug :appcoins-lifecycle:assembleDebug
 ./gradlew publish bintrayUpload
